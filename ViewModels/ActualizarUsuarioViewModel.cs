c5bf807288a246854de68a67e22534a78d1b6689
Freddy Alejandro <freddy19962000@hotmail.com>
2023-01-20 04:37:35 -0500

    Add project files.

---
+++ ViewModels/ActualizarUsuarioViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ViewModels
+{
+    [DataContract]
+    public class ActualizarUsuarioViewModel : CrearUsuarioViewModel
+    {
+        [DataMember]
+        [Required(ErrorMessage="El id del usuario es necesario")]
+        public int UsuarioId { get; set; }
+    }
+}
---
+++ ViewModels/CrearUsuarioViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ViewModels
+{
+    [DataContract]
+    public class CrearUsuarioViewModel
+    {
+        [DataMember]
+        [Required(ErrorMessage = "El nombre del usuario es obligatorio")]
+        [MaxLength(100,ErrorMessage = "El nombre del usuario no debe exceder mas de 100 caracteres")]
+        [MinLength(2,ErrorMessage = "El nombre del usuario no puede contener un solo caracter")]
+        public virtual string Nombre { get; set; }
+        
+        [DataMember]
+        [Required(ErrorMessage = "Por favor selecciona el sexo del usuario")]
+        public virtual string Sexo { get; set; }
+        
+        [DataMember]
+        [Required(ErrorMessage = "Selecciona la fecha de nacimiento del usuario")]
+        public virtual DateTime FechaNacimiento { get; set; }
+    }
+}
